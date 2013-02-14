dfe5e6127376a6204010b566a73ac8f8c91d3442
LEETAEHO <tae1560@gmail.com>
2013-02-14 16:15:33 +0900

    Delete Ranking substituted by requestFriends

--- src/contest/lab/gala/callback/RankingCallback.java
+++
@@ -1,9 +0,0 @@
-package contest.lab.gala.callback;
-
-import java.util.ArrayList;
-
-import contest.lab.gala.data.RankingData;
-
-public interface RankingCallback {
-	public void didSuccessGetRanking(ArrayList<RankingData> array);
-}
--- src/contest/lab/gala/data/RankingData.java
+++
@@ -1,8 +0,0 @@
-package contest.lab.gala.data;
-
-public class RankingData {
-	public String id;
-	public int numOfCharacter;
-	public int ranking;
-	public int numOfWins;
-}
